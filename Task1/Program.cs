90ce92143fc58e135752c702d7fcfb7ebdb3104b
Alex <gusena.16>
2024-01-05 08:45:03 +0300

    Задача 1

---
+++ Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте значения M и N. Напишите
+// программу, которая выведет все натуральные числа
+// в промежутке от M до N. Использовать рекурсию, не
+// использовать циклы.
+// Пример
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+void SumInterval (int m, int n)
+{
+    if (m > n) return;
+    else 
+    {
+        System.Console.Write(m + " ");
+        m++;
+        SumInterval(m, n);
+    }
+}
+
+System.Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+SumInterval(M, N);
